f8d4ac461d210b42bc1fde7048cc85d4cd8514a4
zmwxiaoming <563652043@qq.com>
2015-02-01 22:47:50 +0800

    o(*≧▽≦)ツ

---
+++ src/main/java/idea/util/ConstantUtil.java
@@ -0,0 +1,9 @@
+package idea.util;
+
+import java.io.Serializable;
+
+public class ConstantUtil implements Serializable {
+	private static final long serialVersionUID = -8590844343788255616L;
+	private String STARTING= "xxt";       //出发地:小西天
+	private String OPERATOR = "zmw";//经手人:赵明威
+}
